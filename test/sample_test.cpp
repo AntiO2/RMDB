968e94ac06d3729781ee4cf8fb5c30368eb856da
AntiO2 <antio2@qq.com>
2023-07-13 00:03:13 +0800

    feat(tool) 增加了日志工具，请查看sample test和logger.h

--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -1,17 +1,44 @@
 //
 // Created by Anti on 2023/7/9.
 //
+#include <string>
 #include <gtest/gtest.h>
+#include "logger.h"
 
+TEST(LOGGER_TEST, INFO_TEST) {
+    std::string msg = "Test Info Logger";
+    int code = 1;
+    size_t dota = 825;
+    LOG_INFO("code: %d", code);
+    LOG_INFO("this is test info %zu", dota);
+    LOG_INFO("msg: %s", msg.c_str());
+}
+TEST(LOGGER_TEST, DEBUG_TEST) {
+    std::string msg = "Test Debug Logger";
+    int code = 1;
+    size_t dota = 825;
+    LOG_DEBUG("code: %d", code);
+    LOG_DEBUG("this is debug info %zu", dota);
+    LOG_DEBUG("msg: %s", msg.c_str());
+}
+TEST(LOGGER_TEST, ERROR_TEST) {
+    std::string msg = "Test Error Logger";
+    int code = 1;
+    size_t dota = 825;
+    LOG_ERROR("code: %d", code);
+    LOG_ERROR("this is error info %zu", dota);
+    LOG_ERROR("msg: %s", msg.c_str());
+}
 TEST(SAMPLE_TEST,CHECKPOINT_1) {
     EXPECT_EQ(true, true);
     EXPECT_NE(true,false);
 }
 
-TEST(SAMPLE_TEST, SET_TEST) {
+TEST(SAMPLE_TEST, DISABLED_SET_TEST) {
     std::unordered_map<int,bool> map;
     map.emplace(1,true);
     EXPECT_EQ(map.size(), 1);
     map.emplace(1,true);
     EXPECT_EQ(map.size(), 1);
 }
+

7f387d5cf84d7286ccc311d8025a722248a6267c
AntiO2 <antio2@qq.com>
2023-07-23 22:32:13 +0800

    chore(executor) 更新测试文件

--- test/gen_sql_block_join_test.cpp
+++ test/gen_sql_block_join_test.cpp
@@ -7,11 +7,13 @@
 
 TEST(BLOCK_JOIN,GEN_SQL) {
   fmt::print("create table t ( id int , t_name char (500));\ncreate table d (id int,d_name char(500));\n"); // 这里char填多一点，方便测试溢出的情况
-  for(int i = 1; i <= 50; i++) {
+  for(int i = 50; i > 0; i--) {
     fmt::print("insert into t values({},'');\n",i);
   }
-  for(int i = 1; i <= 50; i++) {
+  for(int i = 50; i > 0; i--) {
     fmt::print("insert into d values({},'');\n",i);
   }
-  fmt::print("select * from t, d;");
+  fmt::print("select * from t, d;\n");
+
+  fmt::print("drop table t;\ndrop table d;\n");
 }

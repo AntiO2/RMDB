8ae3212b00789a9bda726f460d6f57b835dc2b57
Bri1987 <1216754835@qq.com>
2023-07-23 17:34:16 +0800

    补充sort

--- src/execution/execution_sort.h
+++ src/execution/execution_sort.h
@@ -41,23 +41,56 @@ class SortExecutor : public AbstractExecutor {
         used_tuple.clear();
     }
 
-    void beginTuple() override { 
-        for( prev_->beginTuple(); !prev_->is_end() ; prev_->nextTuple()){
+    bool compare(const std::unique_ptr<RmRecord> &lhs, const std::unique_ptr<RmRecord> &rhs)
+    {
+        for( auto &order_col : order_cols){
+            auto colMeta = *get_col(prev_->cols(), order_col.tab_col);
 
+            int res = value_compare(lhs->data + colMeta.offset, rhs->data + colMeta.offset, colMeta.type, colMeta.len);
+            //ia < ib，则返回 -1; ia > ib，则返回 1
+            //ia == ib，则返回 0
+            if((res == -1 && !order_col.is_desc_) || (res == 1 && order_col.is_desc_))
+                return true;
+            else if(res == 0)
+                continue;
+            else
+                return false;
         }
     }
 
-    void nextTuple() override {
+    void beginTuple() override {
+        used_tuple.clear();
+        prev_->beginTuple();
+        if(prev_->is_end()) {
+            is_end_ = true;
+        }
+        //先遍历一遍，获取到所有record, 再进行排序
+        for( ; !prev_->is_end() ; prev_->nextTuple()){
+            tuples.push_back(prev_->Next());
+        }
 
+        std::sort(tuples.begin(), tuples.end(), [this](const std::unique_ptr<RmRecord> &lhs, const std::unique_ptr<RmRecord> &rhs) {
+            return compare(lhs, rhs);
+        });
+        tuple_num = 0;
+    }
+
+    void nextTuple() override {
+        prev_->nextTuple();
+        if(prev_->is_end()) {
+            is_end_ = true;
+        }
+        tuple_num++;
     }
 
     std::unique_ptr<RmRecord> Next() override {
         if(is_end()) {
             return nullptr;
         }
+        return std::move(tuples[tuple_num]);
     }
 
-    bool is_end() const override {
+    [[nodiscard]] bool is_end() const override {
         if((limit > 0 && tuple_num == limit))
             return true;
         return is_end_;
